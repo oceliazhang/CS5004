81bf63049688d71ed25a802a917d7ab1f799ff75
oceliazhang <oceliaaa@163.com>
2023-02-01 20:50:05 -0800

    final version

--- HW1/src/FractionImpl.java
+++ HW1/src/FractionImpl.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Objects;
 
 public class FractionImpl implements Fraction {
@@ -64,8 +63,7 @@ public class FractionImpl implements Fraction {
      */
     @Override
     public double toDouble() {
-        double result = (double) numerator / denominator;
-        return result;
+        return (double) numerator / denominator;  // cast to double
     }
 
     /**
@@ -94,9 +92,10 @@ public class FractionImpl implements Fraction {
      */
     @Override
     public Fraction add(Fraction other) {
+        // using cross-multiplication
         int sumDenominator = this.denominator * other.getDenominator();
         int sumNumerator = this.numerator * other.getDenominator()
-                           + other.getNumerator() * this.denominator;
+                + other.getNumerator() * this.denominator;
         return new FractionImpl(sumNumerator, sumDenominator);
     }
 
@@ -108,16 +107,10 @@ public class FractionImpl implements Fraction {
      */
     @Override
     public int compareTo(Fraction other) {
+        // using cross-multiplication
         int compThis = this.numerator * other.getDenominator();
         int compOther = other.getNumerator() * this.denominator;
-        if (compThis > compOther) {
-            return 1;
-        } else if (compThis < compOther) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
+        return Integer.compare(compThis, compOther);
 
     }
 
@@ -147,64 +140,8 @@ public class FractionImpl implements Fraction {
                 + (numerator / gcd) + " / " + (denominator / gcd);
     }
 
-    public static void main(String[] args) {
-        FractionImpl myFraction = new FractionImpl(4,8);
-        System.out.println(myFraction.toDouble());
-    }
-
     /**
-     * Indicates whether some other object is "equal to" this one.
-     * <p>
-     * The {@code equals} method implements an equivalence relation
-     * on non-null object references:
-     * <ul>
-     * <li>It is <i>reflexive</i>: for any non-null reference value
-     *     {@code x}, {@code x.equals(x)} should return
-     *     {@code true}.
-     * <li>It is <i>symmetric</i>: for any non-null reference values
-     *     {@code x} and {@code y}, {@code x.equals(y)}
-     *     should return {@code true} if and only if
-     *     {@code y.equals(x)} returns {@code true}.
-     * <li>It is <i>transitive</i>: for any non-null reference values
-     *     {@code x}, {@code y}, and {@code z}, if
-     *     {@code x.equals(y)} returns {@code true} and
-     *     {@code y.equals(z)} returns {@code true}, then
-     *     {@code x.equals(z)} should return {@code true}.
-     * <li>It is <i>consistent</i>: for any non-null reference values
-     *     {@code x} and {@code y}, multiple invocations of
-     *     {@code x.equals(y)} consistently return {@code true}
-     *     or consistently return {@code false}, provided no
-     *     information used in {@code equals} comparisons on the
-     *     objects is modified.
-     * <li>For any non-null reference value {@code x},
-     *     {@code x.equals(null)} should return {@code false}.
-     * </ul>
-     *
-     * <p>
-     * An equivalence relation partitions the elements it operates on
-     * into <i>equivalence classes</i>; all the members of an
-     * equivalence class are equal to each other. Members of an
-     * equivalence class are substitutable for each other, at least
-     * for some purposes.
-     *
-     * @param obj the reference object with which to compare.
-     * @return {@code true} if this object is the same as the obj
-     * argument; {@code false} otherwise.
-     * @implSpec The {@code equals} method for class {@code Object} implements
-     * the most discriminating possible equivalence relation on objects;
-     * that is, for any non-null reference values {@code x} and
-     * {@code y}, this method returns {@code true} if and only
-     * if {@code x} and {@code y} refer to the same object
-     * ({@code x == y} has the value {@code true}).
-     * <p>
-     * In other words, under the reference equality equivalence
-     * relation, each equivalence class only has a single element.
-     * @apiNote It is generally necessary to override the {@link #hashCode() hashCode}
-     * method whenever this method is overridden, so as to maintain the
-     * general contract for the {@code hashCode} method, which states
-     * that equal objects must have equal hash codes.
-     * @see #hashCode()
-     * @see HashMap
+     * Override equals
      */
     @Override
     public boolean equals(Object obj) {
@@ -214,5 +151,3 @@ public class FractionImpl implements Fraction {
         return false;
     }
 }
-
-
