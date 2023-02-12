666434937182604def02253ea4d56c00966a7f82
oceliazhang <oceliaaa@163.com>
2023-02-11 21:53:12 -0800

    add javadocs

--- lab02/src/PetRock.java
+++ lab02/src/PetRock.java
@@ -1,8 +1,12 @@
 public class PetRock {
     private String name;
     private boolean happy = false;
-    private double weight = 50.8;
 
+    /**
+     * Constructs a PetRock object with a string name
+     * @param name the name of the pet rock
+     * if pass an empty string as name, throw IllegalArgumentException
+     */
     public PetRock(String name) {
         if (name.isEmpty()) {
             throw new IllegalArgumentException();
@@ -10,18 +14,34 @@ public class PetRock {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the object
+     * @return name of object
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns a boolean value showing whether the object is happy or not
+     * @return boolean value showing whether the object is happy or not
+     */
     public boolean isHappy() {
         return happy;
     }
 
+    /**
+     * if we play with rock, it will be happy
+     */
     public void playWithRock() {
         happy = true;
     }
 
+    /**
+     * if the rock is happy, returns the happy message "I'm happy!"
+     * if not happy, throws IllegalStateException
+     * @return happy message "I'm happy!"
+     */
     public String getHappyMessage() {
         if (!happy) {
             throw new IllegalStateException();
@@ -29,10 +49,18 @@ public class PetRock {
         return "I'm happy!";
     }
 
+    /**
+     * return the weight of the rock
+     * @return the weight of the rock
+     */
     public double getWeight() {
-        return weight;
+        return 50.8;
     }
 
+    /**
+     * toString() method
+     * @return "Hi, the pet rock " + the rock's name + " weighs 50.8 grams"
+     */
     public String toString() {
         return "Hi, the pet rock " + this.name + " weighs 50.8 grams";
     }
--- lab02/tests/PetRockTest.java
+++ lab02/tests/PetRockTest.java
@@ -3,58 +3,86 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PetRockTest {
+    // create rocky object for future test
     private PetRock rocky = new PetRock("Rocky");
 
+    /**
+     * Test getName() method
+     */
     @Test
-    public void getName() {
+    public void testGetName() {
         assertEquals("Rocky", rocky.getName());
     }
 
+    /**
+     * Test rock is not happy when it's initialized
+     */
     @Test
     public void testUnhappyToStart() {
         assertFalse(rocky.isHappy());
     }
 
+    /**
+     * Test playWithRock() method
+     */
     @Test
-    public void testHappyAfterPlay() {
+    public void testPlayWithRock() {
         rocky.playWithRock();
         assertTrue(rocky.isHappy());
     }
 
+    /**
+     * Test getHappyMessage() to get exception
+     */
     @Test (expected = IllegalStateException.class)
-     public void nameFail() {
+    public void testGetHappyMessageFalse() {
         rocky.getHappyMessage();
     }
 
+    /**
+     * Test getHappyMessage()
+     */
     @Test
-    public void name() {
+    public void testGetHappyMessage() {
         rocky.playWithRock();
         String msg = rocky.getHappyMessage();
         assertEquals("I'm happy!", msg);
     }
 
+    /**
+     * Test constructor when passing an empty string
+     */
     @Test (expected = IllegalArgumentException.class)
-    public void nameEmpty() {
+    public void testConstructor() {
         new PetRock("");
     }
 
+    /**
+     * Test getWeight() method using assertEquals
+     */
     @Test
-    public void getWeightEquals() {
+    public void testGetWeightEquals() {
         // assertEquals asserts that two objects are equal.
         // this test will pass.
         assertEquals(50.8, rocky.getWeight(), 1e-15);
     }
 
-    @Test
-    public void getWeightSame() {
-        // assertSame asserts that two objects refer to the same object.
-        // this test will fail.
-        assertSame(50.8, rocky.getWeight());
-    }
+//    /**
+//     * Test getWeight() method using assertSame
+//     */
+//    @Test
+//    public void testGetWeightSame() {
+//        // assertSame asserts that two objects refer to the same object.
+//        // this test will fail.
+//        assertSame(50.8, rocky.getWeight());
+//    }
 
+    /**
+     * Test toString() method
+     */
     @Test
     public void testToString() {
         assertEquals("Hi, the pet rock Rocky weighs 50.8 grams",
-                      rocky.toString());
+                rocky.toString());
     }
 }
